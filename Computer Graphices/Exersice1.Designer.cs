f4588b0873eb2556a64f3c5867bdfc7e417bf1be
Bounleuam <bounleuam77@gmail.com>
2024-03-21 14:16:53 +0700

    Add new from1

---
+++ Computer Graphices/Exersice1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Computer_Graphices
+{
+	partial class Exersice1
+	{
+		/// <summary>
+		///  Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		///  Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		///  Required method for Designer support - do not modify
+		///  the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			button1 = new Button();
+			btnDraw = new Button();
+			btnColor = new Button();
+			numericUpDown1 = new NumericUpDown();
+			label1 = new Label();
+			((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
+			SuspendLayout();
+			// 
+			// button1
+			// 
+			button1.Location = new Point(628, 38);
+			button1.Name = "button1";
+			button1.Size = new Size(121, 42);
+			button1.TabIndex = 0;
+			button1.Text = "Draw";
+			button1.UseVisualStyleBackColor = true;
+			button1.Click += button1_Click;
+			// 
+			// btnDraw
+			// 
+			btnDraw.Location = new Point(628, 95);
+			btnDraw.Name = "btnDraw";
+			btnDraw.Size = new Size(121, 42);
+			btnDraw.TabIndex = 0;
+			btnDraw.Text = "Delete";
+			btnDraw.UseVisualStyleBackColor = true;
+			btnDraw.Click += btnDraw_Click;
+			// 
+			// btnColor
+			// 
+			btnColor.Location = new Point(628, 226);
+			btnColor.Name = "btnColor";
+			btnColor.Size = new Size(121, 42);
+			btnColor.TabIndex = 0;
+			btnColor.Text = "button1";
+			btnColor.UseVisualStyleBackColor = true;
+			btnColor.Click += btnColor_Click;
+			// 
+			// numericUpDown1
+			// 
+			numericUpDown1.Location = new Point(628, 174);
+			numericUpDown1.Name = "numericUpDown1";
+			numericUpDown1.Size = new Size(121, 27);
+			numericUpDown1.TabIndex = 1;
+			numericUpDown1.Value = new decimal(new int[] { 9, 0, 0, 0 });
+			numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(559, 176);
+			label1.Name = "label1";
+			label1.Size = new Size(63, 20);
+			label1.TabIndex = 2;
+			label1.Text = "Pen Size";
+			// 
+			// Exersice1
+			// 
+			AutoScaleDimensions = new SizeF(8F, 20F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 450);
+			Controls.Add(label1);
+			Controls.Add(numericUpDown1);
+			Controls.Add(btnColor);
+			Controls.Add(btnDraw);
+			Controls.Add(button1);
+			Name = "Exersice1";
+			Text = "Form1";
+			Paint += Exersice1_Paint;
+			((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
+
+		#endregion
+
+		private Button button1;
+		private Button btnDraw;
+		private Button btnColor;
+		private NumericUpDown numericUpDown1;
+		private Label label1;
+	}
+}
---
+++ Computer Graphices/Exersice1.cs
@@ -0,0 +1,71 @@
+﻿
+using System.Drawing;
+namespace Computer_Graphices
+{
+	public partial class Exersice1 : Form
+	{
+		Graphics g;
+		Pen p;
+		float ps;
+		Brush b;
+
+		public Exersice1()
+		{
+			InitializeComponent();
+			ps = 1.0f;
+			b = new SolidBrush(Color.Orange);
+			btnColor.BackColor = ((SolidBrush)b).Color;
+		}
+
+
+		private void btnDraw_Click(object sender, EventArgs e)
+		{
+			g = this.CreateGraphics();
+			p = new Pen(Color.BlueViolet, ps);
+			g.DrawRectangle(p, 50, 50, 200, 200);
+			g.FillRectangle(b, 51, 51, 198, 198);
+			g.DrawEllipse(p, 50, 250, 100, 200);
+			g.FillEllipse(b, 51, 251, 98, 198);
+			Font t = new Font("Saysettha OT", 16);
+			g.DrawString("ວິ ຊາຄອມພິ ວເຕີ ກຣາຟຟິ ກ", t, Brushes.Black, 200, 300);
+			g.Dispose();
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			g = this.CreateGraphics();
+			g.Clear(Color.WhiteSmoke);
+			g.Dispose();
+		}
+
+		private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+		{
+			ps = (float)numericUpDown1.Value;
+			Invalidate();
+		}
+
+		private void btnColor_Click(object sender, EventArgs e)
+		{
+			ColorDialog clr = new ColorDialog();
+			if (clr.ShowDialog() == DialogResult.OK)
+			{
+				btnColor.BackColor = clr.Color;
+				b = new SolidBrush(clr.Color);
+				Invalidate();
+			}
+		}
+
+		private void Exersice1_Paint(object sender, PaintEventArgs e)
+		{
+			g = e.Graphics;
+			p = new Pen(Color.BlueViolet, ps);
+			g.DrawRectangle(p, 50, 50, 200, 200);
+			g.FillRectangle(b, 51, 51, 198, 198);
+			g.DrawEllipse(p, 50, 250, 100, 200);
+			g.FillEllipse(b, 51, 251, 98, 198);
+			Font t = new Font("Saysettha OT", 16);
+			g.DrawString("ວິ ຊາຄອມພິ ວເຕີ ກຣາຟຟິ ກ", t, Brushes.Black, 200, 300);
+			g.Dispose();
+		}
+	}
+}
